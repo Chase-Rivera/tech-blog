041a93100b4745354cb35fec7b6fbdedadc41671
Chase Rivera <achaserivera@gmail.com>
2021-10-05 16:56:00 -0700

    built out seed.js will return to complete when finished with other seed files

---
+++ seeds/seed.js
@@ -0,0 +1,28 @@
+const sequelize = require('../config/connection');
+const {User, Post, comment} = require('../models');
+const userData = require()
+const commentDtata = require()
+const postData = require()
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true});
+
+    const user = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+    for (const post of postData) {
+        await Post.create({
+            ...post,
+            user_id: users[Math.floor(Math.random() * user.length)].id,
+        });
+    }
+    for (const comment of commentData) {
+        await Comment.create({
+            ...comment,
+            user_id: user[Math.floor(Math.random() * user.length)].id,
+        });
+    }
+    process.exit(0);
+};
+seedDatabase();
