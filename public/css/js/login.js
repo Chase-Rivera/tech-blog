8acdb000fb8107afffab197f46a04e8028905f73
Chase Rivera <achaserivera@gmail.com>
2021-10-05 16:09:45 -0700

    completed log in

--- public/css/js/login.js
+++ public/css/js/login.js
@@ -18,3 +18,28 @@ const loginForm = async (event) => {
         }
     }
 };
+
+const signupForm = async (event) => {
+    event.preventDefault();
+
+    const user = document.querySelector('#user-sinup').nodeValue.trim();
+    const email = document.querySelector('#email-signup').nodeValue.trim();
+    const password = document.querySelector('#password-signup').nodeValue.trim();
+
+    if (user && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ user, email, password }),
+            headers: {'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.replace('/post');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginForm);
