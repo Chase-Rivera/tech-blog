c2d83256e4e2140e7954e676d0b6eee2cdec718e
Chase Rivera <achaserivera@gmail.com>
2021-10-05 16:25:43 -0700

    crated comment.js to handle new comments

---
+++ public/comment.js
@@ -0,0 +1,25 @@
+const newComment = async (event) => {
+    event.preventDefault();
+
+    const comment = document.querySelector('#newComment').nodeValue.trim();
+    const post_id = document.querySelector('.postdisplay').getAttribute('post-id');
+
+    if (comment && post-id) {
+        const response = await fetch(`/api/comment`, {
+            method: 'POST',
+            body: JSON.stringify({ comment, post_id}),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        });
+        if (response.ok) {
+            document.location.replace(`/post/${post_id}`);
+        } else {
+            alert('Unable to post comment');
+        }
+    }
+};
+
+document
+    .querySelector('.new-comment')
+    .addEventListener('submit', newComment);
