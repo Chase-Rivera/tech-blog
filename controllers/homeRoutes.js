98ff590b402070deb2f49c3f4969b6cb95f43c8f
Chase Rivera <achaserivera@gmail.com>
2021-11-10 19:18:27 -0800

    changed profile get route to dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -92,7 +92,7 @@ router.get('/posteditor/:id', async (req, res) => {
     }
   });
 
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
@@ -101,7 +101,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render('dashboard', {
       ...user,
       logged_in: true
     });
